68d032a636035e7719cad7697267a7d843677835
gd2212 <gscorreia89@gmail.com>
2025-01-15 22:28:34 +0000

    Add decontam plots

--- Code/16S processing/DecontamScripts/decontam-script.R
+++ Code/16S processing/DecontamScripts/decontam-script.R
@@ -40,19 +40,36 @@ write.csv(freqContaminants, 'decontam-Filtered-Frequence.csv')
 
 ## Plots 
 library(ggplot2)
+
+# Prevalence filter plot
+ps.pa <- transform_sample_counts(phyloseqDataset, function(abund) 1*(abund > 0))
+ps.pa.neg <- prune_samples(sample_data(ps.pa)$SampleType == "Control", ps.pa)
+ps.pa.pos <- prune_samples(sample_data(ps.pa)$SampleType == "Sample", ps.pa)
+# Make data.frame of prevalence in positive and negative samples
+df.pa <- data.frame(pa.pos=taxa_sums(ps.pa.pos), pa.neg=taxa_sums(ps.pa.neg),
+                    contaminant=contamdf.prev$contaminant)
+decontamPlotA <- ggplot(data=df.pa, aes(x=pa.neg, y=pa.pos, color=contaminant)) + geom_point() +
+  xlab("Prevalence (Negative Controls)") + ylab("Prevalence (True Samples)") + theme_light()
+
+ggsave('./DecontamPlot_Prevalence.png', decontamPlotB)
+
+
+# Frequency filter plot
+set.seed(100)
+decontamPlotB <- plot_frequency(subset_samples(phyloseqDataset, !is.na(`Total.16s.rRNA.copy.per.mL`)), 
+                                taxa_names(subset_samples(phyloseqDataset, !is.na(`Total.16s.rRNA.copy.per.mL`)))[sample(which(contamdf.freq$contaminant), 10)], 
+                                conc="Total.16s.rRNA.copy.per.mL") +                   
+xlab("Total 16s rRNA") + theme_light()
+
+ggsave('./DecontamPlot_Frequency.png', decontamPlotB)
+
 df <- as.data.frame(sample_data(phyloseqDataset)) # Put sample_data into a ggplot-friendly data.frame
 df$LibrarySize <- sample_sums(phyloseqDataset)
 df <- df[order(df$LibrarySize),]
 df$Index <- seq(nrow(df))
 decontamPlotA <- ggplot(data=df, aes(x=Index, y=LibrarySize, color=SampleType)) + geom_point()
-
 ggsave('./DecontamPlotA.png', decontamPlotA)
 
-set.seed(100)
-decontamPlotB <- plot_frequency(subset_samples(phyloseqDataset, !is.na(`Total.16s.rRNA.copy.per.mL`)), taxa_names(subset_samples(phyloseqDataset, !is.na(`Total.16s.rRNA.copy.per.mL`)))[sample(which(!contamdf.freq$contaminant), 81)], conc="Total.16s.rRNA.copy.per.mL") +                   
-  xlab("Total 16s rRNA")
-
-ggsave('./DecontamPlotB.png', decontamPlotB)
 
 taxaToFilter <- c(rownames(contamdf.prev[contamdf.prev$contaminant == TRUE, ]), rownames(contamdf.freq[contamdf.freq$contaminant ==TRUE, ]))
 
@@ -63,3 +80,4 @@ phyloDFiltered <- prune_taxa(taxaSelection, phyloseqDataset)
 otu_table(phyloDFiltered) %>%   as("matrix") %>%
   make_biom() %>%
   write_biom("./3361-ASV-Outputs-V1MixedV2-SILVA/table-decontam-filtered.biom")
+
